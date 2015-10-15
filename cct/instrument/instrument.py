0e3d25b2a58b8512ee755bebb4d78325726c7574
andris <andris@asaxs>
2015-10-15 08:07:07 +0200

    Work on instrument/instrument.py

--- cct/instrument/instrument.py
+++ cct/instrument/instrument.py
@@ -1,3 +1,7 @@
+from ..devices.detector import Pilatus
+from ..devices.xray_source import GeniX
+from ..devices.motor import TMCM551, TMCM6110
+from ..devices.vacuumgauge import VacuumGauge
 import json
 import os
 
@@ -13,9 +17,49 @@ class Instrument(GObject.GObject):
         self.detector = None
         self.motorcontrollers = None
         self.motors = None
-        self.configfile = os.path.join(self.configdir, 'cct.conf')
-
-    def save_state(self, file):
+        self.configfile = os.path.join(self.configdir, 'cct.json')
+        self._initialize_config()
+        
+    def _initialize_config(self):
+        """Create a sane configuration in `self.config` from sratch."""
+        self.config = {}
+        self.config['connections'] = {}
+        self.config['connections']['xray_source'] = {'host':'genix.credo',
+                                                     'port':502,
+                                                     'timeout':1,
+                                                     'name':'pilatus'}
+        self.config['connections']['detector'] = {'host':'pilatus300k.credo',
+                                                  'port':41234,
+                                                  'timeout':0.01,
+                                                  'poll_timeout':0.01,
+                                                  'name':'genix'}
+        self.config['connections']['vacgauge'] = {'host':'devices.credo',
+                                                  'port':2006,
+                                                  'timeout':0.01,
+                                                  'poll_timeout':0.01,
+                                                  'name':'tpg201'}
+        self.config['connections']['motorcontrollers'] = {}
+        self.config['connections']['motorcontrollers']['tmcm351a'] = {
+            'host':'devices.credo',
+            'port':2003,
+            'timeout':0.01,
+            'poll_timeout':0.01,
+            'name':'tmcm351a'}
+        self.config['connections']['motorcontrollers']['tmcm351b'] = {
+            'host':'devices.credo',
+            'port':2004,
+            'timeout':0.01,
+            'poll_timeout':0.01,
+            'name':'tmcm351b'}
+        self.config['connections']['motorcontrollers']['tmcm6110'] = {
+            'host':'devices.credo',
+            'port':2005,
+            'timeout':0.01,
+            'poll_timeout':0.01,
+            'name':'tmcm6110'}
+            
+    
+    def save_state(self):
         dic = {'xray_source': self.xray_source._save_state(),
                'detector': self.detector._save_state()}
         for m in self.motorcontrollers:
@@ -23,6 +67,12 @@ class Instrument(GObject.GObject):
         with open(self.configfile, 'wt', encoding='utf-8') as f:
             json.dump(dic, f)
 
-    def load_state(self, file):
-        with open(self.configfile, 'rt', encoding='utf-8') as f:
-            json.load(f)
+    def load_state(self):
+        try:
+            with open(self.configfile, 'rt', encoding='utf-8') as f:
+                dic=json.load(f)
+        except IOError:
+            return
+        self.config['connection
+        dic['connections']['xray_source']['host']
+            
