37fd4321e5a45313070018a78ec88d628e16d067
Andras Wacha <awacha@gmail.com>
2015-11-17 16:17:46 +0100

    Improvements in indicators

--- cct/gui/core/indicator.py
+++ cct/gui/core/indicator.py
@@ -1,5 +1,7 @@
-from gi.repository import Gtk
 import pkg_resources
+from gi.repository import Gtk
+from gi.repository import Pango
+
 cssprovider=Gtk.CssProvider()
 cssprovider.load_from_path(pkg_resources.resource_filename('cct','resource/css/indicatorcolors.css'))
 
@@ -16,10 +18,16 @@ class Indicator(Gtk.Box):
         if 'orientation' not in kwargs:
             self.set_orientation(Gtk.Orientation.VERTICAL)
         self._label=Gtk.Label(label=label)
+        #        self._label.set_hexpand(True)
+        #        self._label.set_hexpand_set(True)
         self.pack_start(self._label, True, True, 0)
         self._eventbox=Gtk.EventBox()
         self.pack_start(self._eventbox, True, True, 0)
         self._valuelabel=Gtk.Label(label=str(value))
+        #      self._valuelabel.set_hexpand(False)
+        #      self._valuelabel.set_hexpand_set(False)
+        self._valuelabel.set_ellipsize(Pango.EllipsizeMode.MIDDLE)
+        self._valuelabel.set_max_width_chars(1)
         self._value=value
         self._eventbox.add(self._valuelabel)
         self._eventbox.set_border_width(5)
@@ -27,7 +35,8 @@ class Indicator(Gtk.Box):
         self._eventbox.get_style_context().add_provider(cssprovider, Gtk.STYLE_PROVIDER_PRIORITY_USER)
         self.set_hexpand(True)
         self.set_hexpand_set(True)
-#        self._eventbox.queue_draw()
+        self._eventbox.queue_draw()
+        #       self.set_size_request(self._label.get_size_request()[0],-1)
 
     def set_label(self, text):
         return self._label.set_text(text)
@@ -38,6 +47,8 @@ class Indicator(Gtk.Box):
     def set_value(self, value, state=None):
         self._value=value
         res= self._valuelabel.set_text(str(value))
+        self._eventbox.set_tooltip_text(self._label.get_text() + ': ' + value)
+        self._valuelabel.set_tooltip_text(self._label.get_text() + ': ' + value)
         if state is not None:
             self.set_state(state)
         return res
