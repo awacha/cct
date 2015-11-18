614c64fc85a7e68d4aacce375f51f47d2d1744e9
Andras Wacha <awacha@gmail.com>
2015-11-18 16:20:15 +0100

    Forgot to add commands/__init__.py

--- cct/core/commands/__init__.py
+++ cct/core/commands/__init__.py
@@ -1,2 +1,2 @@
 from .command import Command, CommandError, CommandTimeoutError
-from . import detector, device, jump, motor, scan, transmission, vacuumgauge, xray_source, script
+from . import circulator, command, detector, device, jump, motor, scan, script, transmission, vacuumgauge, xray_source
