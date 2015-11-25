awacha/cct

commit 2adedc35d93151308017a17a8adf70b708233365

Trying to implement reconnection to devices on failure.

cct/core/devices/device.py
@@ -166,6 +166,12 @@ class Device(GObject.GObject):
                 self._queue_to_backend.get_nowait()
             except queue.Empty:
                 break
+        self._outstanding_telemetry = False
+        self._properties = {'_status': 'Disconnected'}
+        self._timestamps = {'_status': time.time()}
+        self._queue_to_backend = multiprocessing.Queue()
+        self._queue_to_frontend = multiprocessing.Queue()
+        self._refresh_requested = {}
         self._background_process = multiprocessing.Process(
             target=self._background_worker, daemon=True, name='Background process for device %s' % self._instancename)
         self._idle_handler = GLib.idle_add(self._idle_worker)
@@ -183,7 +189,14 @@ class Device(GObject.GObject):
             self._background_process.join()
             GLib.source_remove(self._idle_handler)
         finally:
-            del self._idle_handler
+            try:
+                del self._idle_handler
+            except AttributeError:
+                pass
+            try:
+                del self._background_process
+            except AttributeError:
+                pass
 
     def get_variable(self, name):
         """Get the value of the variable. If you need the most fresh value,
@@ -350,6 +363,7 @@ class Device(GObject.GObject):
             else:
                 raise NotImplementedError(
                     'Unknown command for _background_worker: %s' % cmd)
+        self._logger.info('Background thread ending for device %s' % self._instancename)
 
     def _get_telemetry(self):
         return {'processname': multiprocessing.current_process().name,
@@ -484,7 +498,10 @@ class Device(GObject.GObject):
         push the appropriate messages into `_queue_to_background` in step 3).
 
         Connection and communication parameters (e.g. host, port, baud rate,
-        timeout etc.) should be given as arguments to this method.
+        timeout etc.) should be given as arguments to this method. These have
+        also to be saved somewhere, where the `reconnect_device()` method can
+        find it. Preferably in the instance attribute `_connection_parameters`,
+        the implementation of which depends on the subclass.
 
         If an exception happens at any step, the state of the device has to be
         returned to disconnected. If the exception happens in 4), 
@@ -504,6 +521,13 @@ class Device(GObject.GObject):
         failure or because of the remote end. In this case this function is
         called on a queue message from the backend.
         """
+        raise NotImplementedError
+
+    def reconnect_device(self):
+        """Try to reconnect the device after a spontaneous disconnection.
+        This method must simply call the `connect_device` method, with
+        the connection parameters stored by a previous invocation of the
+        latter."""
         raise NotImplementedError
 
     def _initialize_after_connect(self):
@@ -574,6 +598,7 @@ class Device_TCP(Device):
         self._logger.debug('Connecting to device: %s:%d' % (host, port))
         if self._get_connected():
             raise DeviceError('Already connected')
+        self._connection_parameters = (host, port, socket_timeout, poll_timeout)
         try:
             self._tcpsocket = socket.create_connection(
                 (host, port), socket_timeout)
@@ -630,6 +655,9 @@ class Device_TCP(Device):
         finally:
             del self._tcpsocket
 
+    def reconnect_device(self):
+        self.connect_device(*self._connection_parameters)
+
     def _send(self, message):
         """Send a message (bytes) to the device"""
         # self._logger.debug('Sending message %s' % str(message))
@@ -674,7 +702,6 @@ class Device_TCP(Device):
                         # remote end hung up on us
                         raise CommunicationError(
                             'Socket has been closed by the remote side')
-                        return
                 if not message:
                     # no message was read, because no message was waiting. Note that
                     # we are not dealing with an empty message, which would signify
@@ -706,6 +733,7 @@ class Device_ModbusTCP(Device):
         self._logger.debug('Connecting to device: %s:%d' % (host, port))
         if self._get_connected():
             raise DeviceError('Already connected')
+        self._connection_parameters = (host, port, modbus_timeout)
         self._modbusclient = ModbusClient(host, port, timeout=modbus_timeout)
         if not self._modbusclient.open():
             raise DeviceError(
@@ -737,6 +765,9 @@ class Device_ModbusTCP(Device):
             self._logger.debug('Closed socket')
         finally:
             del self._modbusclient
+
+    def reconnect_device(self):
+        self.connect_device(*self._connection_parameters)
 
     def _read_integer(self, regno):
         return self._modbusclient.read_holding_registers(regno, 1)[0]

cct/core/instrument/instrument.py
@@ -3,6 +3,7 @@ import logging
 import multiprocessing
 import os
 import resource
+import time
 import traceback
 
 from .motor import Motor
@@ -390,7 +391,20 @@ class Instrument(GObject.GObject):
             self.emit('devices-ready')
 
     def on_disconnect(self, device, because_of_failure):
-        pass
+        if because_of_failure:
+            # attempt to reconnect
+            self._waiting_for_ready = [w for w in self._waiting_for_ready if w != device.name]
+            for i in range(3):
+                try:
+                    device.reconnect_device()
+                    self._waiting_for_ready.append(device.name)
+                    break
+                except Exception as exc:
+                    logger.warning('Exception while reconnecting to device %s: %s, %s' % (
+                    device.name, exc, traceback.format_exc()))
+                    time.sleep(1)  # a blocking sleep. Keep the other parts of this program from accessing the device.
+            if device.name not in self._waiting_for_ready:
+                logger.error('Cannot reconnect to device %s.' % device.name)
 
     def start_services(self):
         self.interpreter = Interpreter(self)

cct/gui/mainwindow.py
@@ -2,6 +2,7 @@ from gi import require_version
 
 require_version('Gtk', '3.0')
 require_version('GtkSource', '3.0')
+require_version('Notify', '0.7')
 from gi.repository import Gio
 from gi.repository import Gtk
 from gi.repository import GLib
@@ -274,6 +275,9 @@ class MainWindow(object):
             self._builder.get_object('command_entry').set_sensitive(idle)
             if self._builder.get_object('execute_button').get_label()=='Execute':
                 self._builder.get_object('execute_button').set_sensitive(idle)
+        if idle:
+            self._builder.get_object('command_entry').set_sensitive(idle)
+            self._builder.get_object('execute_button').set_sensitive(idle)
 
     def on_command_execute(self, button):
         if button.get_label() == 'Execute':

commit 62dfdff9cbc10a35e0a6e3df047dc7ca1f90cd1e

Devices are now initialized correctly (bug introduced in commit 2adedc3). Implemented deadlock recovery in circulator (similar to motors).

cct/core/devices/circulator.py
@@ -174,6 +174,15 @@ class HaakePhoenix(Device_TCP):
                     pass
 
     def _query_variable(self, variablename):
+        if not hasattr(self, '_lastsent'):
+            try:
+                msg = self._sendqueue.get_nowait()
+                self._lastsent = msg
+                Device_TCP._send(self, msg)
+                self._logger.warning('Recovered from a possible deadlock situation!')
+            except queue.Empty:
+                pass
+
         if variablename is None:
             if self._sendqueue.qsize() > 3:
                 # skip autoupdate

cct/core/devices/device.py
@@ -169,8 +169,6 @@ class Device(GObject.GObject):
         self._outstanding_telemetry = False
         self._properties = {'_status': 'Disconnected'}
         self._timestamps = {'_status': time.time()}
-        self._queue_to_backend = multiprocessing.Queue()
-        self._queue_to_frontend = multiprocessing.Queue()
         self._refresh_requested = {}
         self._background_process = multiprocessing.Process(
             target=self._background_worker, daemon=True, name='Background process for device %s' % self._instancename)