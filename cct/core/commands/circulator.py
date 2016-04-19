b8c7f26aebe763ee489b3f5d2b451e761ba196a3
Andras Wacha <awacha@gmail.com>
2016-04-19 15:02:08 +0200

    New command: wait_temp

--- cct/core/commands/circulator.py
+++ cct/core/commands/circulator.py
@@ -1,6 +1,13 @@
+import logging
+import time
+
+from gi.repository import GLib
+
 from .command import Command
 from .script import Script
 
+logger=logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 class StartStop(Command):
     """Start or stop the circulator
@@ -11,7 +18,8 @@ class StartStop(Command):
         <state>: 'start', 'on', True, 1 or 'stop', 'off', False, 0
     """
     name = 'circulator'
-    timeout=5
+    timeout = 5
+
     def execute(self, interpreter, arglist, instrument, namespace):
         if isinstance(arglist[0], str):
             if arglist[0].upper() in ['START', 'ON']:
@@ -69,3 +77,66 @@ class SetTemperature(Script):
     name = 'settemp'
 
     script = "setvar('haakephoenix','setpoint', _scriptargs[0])"
+
+
+class WaitTemperature(Command):
+    """Wait until the temperature stabilizes
+
+    Invocation: wait_temp(<tolerance>, <delay>)
+
+    Arguments:
+        <tolerance>: the radius in which the temperature must reside
+        <delay>: the time interval
+
+    Remarks:
+        In the first phase, the command waits until the temperature read from
+        the controller approaches the set-point by <tolerance>. If this
+        happens, it will wait <delay> seconds, and constantly check if the
+        temperature is still in the tolerance interval. If it gets out of it,
+        the first phase is repeated. If, at the end of the delay,
+        the temperature is still inside the interval, the command finishes.
+    """
+    name = "wait_temp"
+
+    def execute(self, interpreter, arglist, instrument, namespace):
+        self.tolerance = float(arglist[0])
+        self.delay = float(arglist[1])
+        self._require_device(instrument, 'haakephoenix')
+        self._in_tolerance_interval = None
+        self._pulser=GLib.timeout_add(1000,self.pulser)
+        self._instrument = instrument
+
+    def pulser(self):
+        if self._in_tolerance_interval is None:
+            self.emit('pulse', 'Waiting for temperature stability: %.2f °C' %
+                      self._instrument.devices['haakephoenix'].get_variable('temperature'))
+        else:
+            remainingtime = (self.delay - (time.monotonic() - self._in_tolerance_interval))
+            fraction= (time.monotonic()-self._in_tolerance_interval)/self.delay
+            self.emit('progress',
+                      'Temperature stability reached (%.2f °C), waiting for %.0f seconds' % (
+                          self._instrument.devices['haakephoenix'].get_variable('temperature'),
+                          remainingtime),
+                      fraction)
+        self.on_variable_change(self._instrument.devices['haakephoenix'],'temperature',self._instrument.devices['haakephoenix'].get_variable('temperature'))
+        return True
+
+    def on_variable_change(self, device, variablename, newvalue):
+        if variablename == 'temperature':
+            if abs(newvalue - device.get_variable('setpoint')) > self.tolerance:
+                self._in_tolerance_interval = None
+            elif self._in_tolerance_interval is None:
+                self._in_tolerance_interval = time.monotonic()
+            elif (time.monotonic() - self._in_tolerance_interval) > self.delay:
+                self._uninstall_pulse_handler()
+                self._unrequire_device(device.name)
+                GLib.source_remove(self._pulser)
+                self.emit('return', newvalue)
+        return False
+
+    def kill(self):
+        self._uninstall_pulse_handler()
+        self._unrequire_device()
+        GLib.source_remove(self._pulser)
+        self.emit('return', None)
+        return False
