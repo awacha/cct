0503a8919053a4f5ca97c02838435d3b39ba858e
András Wacha <awacha@gmail.com>
2016-10-14 13:28:07 +0200

    ModbusTCP device hardening

--- cct/core/devices/device/device_modbustcp.py
+++ cct/core/devices/device/device_modbustcp.py
@@ -1,14 +1,32 @@
 import time
 
-from pyModbusTCP.client import ModbusClient
+import pyModbusTCP.client
 
 from .backend import DeviceBackend
 from .exceptions import CommunicationError, DeviceError
+
+class ModbusClient(pyModbusTCP.client.ModbusClient):
+    def __init__(self, logger, host=None, port=None, unit_id = None, timeout=None, debug=None, auto_open=None, auto_close=None):
+        super().__init__(host, port, unit_id, timeout, debug, auto_open, auto_close)
+        self.logger = logger
+
+    def __debug_msg(self, msg):
+        if self.__last_error == pyModbusTCP.constants.MB_NO_ERR:
+            self.logger.debug(msg)
+        else:
+            self.logger.error(msg)
+
+    def reset_errors(self):
+        self.__last_error = pyModbusTCP.constants.MB_NO_ERR
+        self.__last_except = pyModbusTCP.constants.EXP_NONE
+
 
 # noinspection PyPep8Naming,PyAbstractClass
 class DeviceBackend_ModbusTCP(DeviceBackend):
     """Device with Modbus over TCP connection.
     """
+
+    modbus_send_retries = 3
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -34,7 +52,7 @@ class DeviceBackend_ModbusTCP(DeviceBackend):
         """
         host, port, modbus_timeout = self.deviceconnectionparameters
         self.logger.debug('Connecting to ModbusTCP device: {}:{:d}'.format(host, port))
-        self._modbusclient = ModbusClient(host, port, timeout=modbus_timeout)
+        self._modbusclient = ModbusClient(self.logger, host, port, timeout=modbus_timeout, debug=True)
         if not self._modbusclient.open():
             self._modbusclient = None
             raise DeviceError(
@@ -60,33 +78,48 @@ class DeviceBackend_ModbusTCP(DeviceBackend):
             self._modbusclient = None
 
     def read_integer(self, regno):
-        self.lasttimes['send'] = time.monotonic()
-        self.counters['outmessages'] += 1
-        result = self._modbusclient.read_holding_registers(regno, 1)
+        result = None
+        for i in range(self.modbus_send_retries):
+            if i>0:
+                self.logger.warning('Retrying read_integer({:d}) operation in ModbusTCP device {}'.format(regno, self.name))
+            self.lasttimes['send'] = time.monotonic()
+            self.counters['outmessages'] += 1
+            result = self._modbusclient.read_holding_registers(regno, 1)
+            if result is not None:
+                break
         if result is None:
-            if not self._modbusclient.is_open():
-                raise CommunicationError('Error reading integer from register #{:d}'.format(regno))
+            raise CommunicationError('Error reading integer from register #{:d}. Error no: {:d}'.format(regno, self._modbusclient.last_error()))
         self.lasttimes['recv'] = time.monotonic()
         self.counters['inmessages'] += 1
         return result[0]
 
     def write_coil(self, coilno, val):
-        self.lasttimes['send'] = time.monotonic()
-        self.counters['outmessages'] += 1
-        result = self._modbusclient.write_single_coil(coilno, val)
+        result = None
+        for i in range(self.modbus_send_retries):
+            if i>0:
+                self.logger.warning('Retrying write_coil({:d}, {}) operation in ModbusTCP device {}'.format(coilno, val, self.name))
+            self.lasttimes['send'] = time.monotonic()
+            self.counters['outmessages'] += 1
+            result = self._modbusclient.write_single_coil(coilno, val)
+            if result is not None:
+                break
         if result is None:
-            if not self._modbusclient.is_open():
-                raise CommunicationError('Error writing {} to coil #{:d}'.format(val, coilno))
+            raise CommunicationError('Error writing {} to coil #{:d}. Error no: {:d}'.format(val, coilno, self._modbusclient.last_error()))
         self.lasttimes['recv'] = time.monotonic()
         self.counters['inmessages'] += 1
 
     def read_coils(self, coilstart, coilnum):
-        self.lasttimes['send'] = time.monotonic()
-        self.counters['outmessages'] += 1
-        result = self._modbusclient.read_coils(coilstart, coilnum)
+        result = None
+        for i in range(self.modbus_send_retries):
+            if i>0:
+                self.logger.warning('Retrying read_coils({:d}, {:d}) operation in ModbusTCP device {}'.format(coilstart, coilnum, self.name))
+            self.lasttimes['send'] = time.monotonic()
+            self.counters['outmessages'] += 1
+            result = self._modbusclient.read_coils(coilstart, coilnum)
+            if result is not None:
+                break
         if result is None:
-            if not self._modbusclient.is_open():
-                raise CommunicationError('Error reading coils #{:d} - #{:d}'.format(coilstart, coilstart + coilnum))
+            raise CommunicationError('Error reading coils #{:d} - #{:d}. Error no: {:d}'.format(coilstart, coilstart + coilnum, self._modbusclient.last_error()))
         self.lasttimes['recv'] = time.monotonic()
         self.counters['inmessages'] += 1
         return result
