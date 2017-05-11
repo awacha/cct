6f9944ae0506c39492f15ea052c570286c2b342d
András Wacha <awacha@gmail.com>
2017-05-11 17:18:14 +0200

    Datareduction

---
+++ cct/qtgui/tools/datareduction/__init__.py
@@ -0,0 +1,1 @@
+from .datareduction import DataReduction
---
+++ cct/qtgui/tools/datareduction/datareduction.py
@@ -0,0 +1,112 @@
+from PyQt5 import QtWidgets, QtCore, QtGui
+from sastool.classes2 import Exposure
+
+from .datareduction_ui import Ui_Form
+from ...core.exposuremodel import HeaderModel
+from ...core.mixins import ToolWindow
+from ....core.services.exposureanalyzer import ExposureAnalyzer
+
+
+class DataReduction(QtWidgets.QWidget, Ui_Form, ToolWindow):
+    def __init__(self, *args, **kwargs):
+        credo = kwargs.pop('credo')
+        QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
+        ToolWindow.__init__(self, credo)
+        self._currentfsn = None
+        self._ea_connection = []
+        self._stoprequested = False
+        self.setupUi(self)
+
+    def setupUi(self, Form):
+        Ui_Form.setupUi(self, Form)
+        self.firstFSNSpinBox.valueChanged.connect(self.onFirstFSNChanged)
+        self.lastFSNSpinBox.valueChanged.connect(self.onLastFSNChanged)
+        self.model = HeaderModel(None, self.credo, 'crd', self.firstFSNSpinBox.value(), self.lastFSNSpinBox.value())
+        self.treeView.setModel(self.model)
+        self.reloadHeaders()
+        self.reloadPushButton.clicked.connect(self.reloadHeaders)
+        self.startStopPushButton.clicked.connect(self.onStartReduction)
+        self.treeView.selectionChanged.connect(self.onSelectionChanged)
+
+    def onSelectionChanged(self):
+        self.startStopPushButton.setEnabled(self.selectedFSNs())
+
+    def onStartReduction(self):
+        if self.startStopPushButton.text()=='Reduce selected exposures':
+            self.setBusy()
+            self._stoprequested = False
+            self.submitNextFSN()
+        else:
+            self._stoprequested = True
+
+    def submitNextFSN(self):
+        ea = self.credo.services['exposureanalyzer']
+        assert isinstance(ea, ExposureAnalyzer)
+        try:
+            self._currentfsn=self.selectedFSNs()[0]
+        except IndexError:
+            self._currentfsn = None
+            self.setIdle()
+            return
+        prefix='crd'
+        ea.submit(
+            self._currentfsn,
+            self.credo.services['filesequence'].exposurefileformat(prefix,self._currentfsn)+'.cbf',
+            prefix,
+            param=self.model.header(self._currentfsn))
+        self.treeView.selectionModel().select(
+            self.model.index(self.model.rowForFSN(self._currentfsn),0),
+            QtCore.QItemSelectionModel.Current | QtCore.QItemSelectionModel.Rows
+        )
+
+    def setBusy(self):
+        super().setBusy()
+        ea=self.credo.services['exposureanalyzer']
+        assert isinstance(ea, ExposureAnalyzer)
+        assert not self._ea_connection
+        self._ea_connection=[ea.connect('datareduction-done', self.reducingDone)]
+        self.inputWidget.setEnabled(False)
+        self.startStopPushButton.setText('Stop data reduction')
+        self.startStopPushButton.setIcon(QtGui.QIcon.fromTheme('process-stop'))
+
+    def cleanup(self):
+        for c in self._ea_connection:
+            self.credo.services['exposureanalyzer'].disconnect(c)
+        self._ea_connection=[]
+        super().cleanup()
+
+    def setIdle(self):
+        super().setIdle()
+        for c in self._ea_connection:
+            self.credo.services['exposureanalyzer'].disconnect(c)
+        self._ea_connection=[]
+        self.inputWidget.setEnabled(True)
+        self.startStopPushButton.setText('Reduce selected exposures')
+        self.startStopPushButton.setIcon(QtGui.QIcon.fromTheme('system-run'))
+
+    def selectedFSNs(self):
+        return sorted([self.model.createIndex(idx.row(), 0).data(QtCore.Qt.DisplayRole)
+                for idx in self.treeView.selectedIndexes()])
+
+    def reducingDone(self, ea:ExposureAnalyzer, prefix:str, fsn:int, ex:Exposure):
+        if self._currentfsn != fsn:
+            return False
+        if self._stoprequested:
+            self.setIdle()
+            return False
+        self.treeView.selectionModel().select(
+            self.model.index(self.model.rowForFSN(fsn),0),
+            QtCore.QItemSelectionModel.Deselect | QtCore.QItemSelectionModel.Rows
+        )
+        self.submitNextFSN()
+
+    def onFirstFSNChanged(self):
+        self.lastFSNSpinBox.setMinimum(self.firstFSNSpinBox.value())
+
+    def onLastFSNChanged(self):
+        self.firstFSNSpinBox.setMaximum(self.lastFSNSpinBox.value())
+
+    def reloadHeaders(self):
+        self.model.fsnfirst=self.firstFSNSpinBox.value()
+        self.model.fsnlast=self.lastFSNSpinBox.value()
+        self.model.reloadHeaders()
