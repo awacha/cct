88fb7ca37ecd9e5da2526c5e9ffae432e7184142
András Wacha <awacha@gmail.com>
2017-10-16 09:25:15 +0200

    New icon for Windows CPT starticon

--- bld.bat
+++ bld.bat
@@ -2,7 +2,7 @@ set MENU_DIR="%PREFIX%"\Menu
 mkdir "%MENU_DIR%"
 copy menu_cpt.json %MENU_DIR%\
 if errorlevel 1 exit 1
-copy cptlogo.png %MENU_DIR%\
+copy cptlogo.ico %MENU_DIR%\
 if errorlevel 1 exit 1
 "%PYTHON%" setup.py install
 if errorlevel 1 exit 1
