8566c292f9120eb267482b14be56c2cde7246b93
András Wacha <awacha@gmail.com>
2018-01-02 17:00:22 +0100

    Some basic commands (echo, print, sleep, set etc.) were moved from commands.device from commands.basic.
    New command: copysample

--- cct/core/commands/__init__.py
+++ cct/core/commands/__init__.py
@@ -1,3 +1,3 @@
-from . import circulator, command, detector, device, jump, mapping, motor, scan, script, transmission, vacuumgauge, \
+from . import basic, circulator, command, detector, device, jump, mapping, motor, scan, script, transmission, vacuumgauge, \
     xray_source
 from .command import Command, CommandError, CommandTimeoutError
