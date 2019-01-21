d5c404402f6396542caf5822ac133a7e689cf5e4
András Wacha <awacha@gmail.com>
2019-01-21 16:21:06 +0100

    Moved conda recipe to a separate directory

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,8 +1,8 @@
 set MENU_DIR="%PREFIX%"\Menu
 mkdir "%MENU_DIR%"
-copy menu_cpt.json %MENU_DIR%\
+copy recipe\menu_cpt.json %MENU_DIR%\
 if errorlevel 1 exit 1
-copy cptlogo.ico %MENU_DIR%\
+copy recipe\cptlogo.ico %MENU_DIR%\
 if errorlevel 1 exit 1
 "%PYTHON%" setup.py install
 if errorlevel 1 exit 1
