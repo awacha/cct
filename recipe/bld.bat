5bc2b2b5d857698a1f1c7bc44434a3a381f968b5
András Wacha <awacha@gmail.com>
2019-11-29 08:49:56 +0100

    New logo for cpt2

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,6 +3,7 @@ mkdir "%MENU_DIR%"
 copy recipe\menu_cpt.json %MENU_DIR%\
 if errorlevel 1 exit 1
 copy recipe\cptlogo.ico %MENU_DIR%\
+copy recipe\cpt2logo.ico %MENU_DIR%\
 if errorlevel 1 exit 1
 "%PYTHON%" setup.py install
 if errorlevel 1 exit 1
