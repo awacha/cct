4947c846d45daace94f96a80334b4bc85226185b
András Wacha <awacha@gmail.com>
2020-08-27 14:22:53 +0200

    PlotCurve enhancement

--- cct/qtgui2/utils/plotcurve.py
+++ cct/qtgui2/utils/plotcurve.py
@@ -1,20 +1,21 @@
 import logging
 from typing import Dict, Any, Tuple, List
 
+import numpy as np
 from PyQt5 import QtWidgets
 from matplotlib.axes import Axes
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT, FigureCanvasQTAgg
 from matplotlib.figure import Figure
-from sastool.classes2 import Curve
 
 from .plotcurve_ui import Ui_Form
+from ...core2.dataclasses import Curve
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
 class PlotCurve(QtWidgets.QWidget, Ui_Form):
-    curves: List[Tuple[Curve, Dict[str, Any], bool, bool]] = None
+    curves: List[Tuple[Curve, Dict[str, Any]]] = None
     figure: Figure
     axes: Axes
     canvas: FigureCanvasQTAgg
@@ -33,60 +34,56 @@ class PlotCurve(QtWidgets.QWidget, Ui_Form):
         self.figure = Figure(figsize=self._figsize, constrained_layout=True)
         self.canvas = FigureCanvasQTAgg(self.figure)
         self.navigationToolbar = NavigationToolbar2QT(self.canvas, self)
-        self.axes = self.figure.add_subplot(1, 1, 1)
+        self.axes = self.figure.add_subplot(self.figure.add_gridspec(1,1)[:,:])
         self.figureVerticalLayout.addWidget(self.navigationToolbar)
         self.figureVerticalLayout.addWidget(self.canvas)
+        self.pixelOrQToolButton.toggled.connect(self.pixelOrQChanged)
+        self.plotTypeComboBox.currentIndexChanged.connect(self.replot)
+        self.showErrorBarsToolButton.toggled.connect(self.replot)
+        self.showGridToolButton.toggled.connect(self.showGrid)
+        self.showLegendToolButton.toggled.connect(self.showLegend)
+        self.showLinesToolButton.toggled.connect(self.replot)
+        self.symbolsTypeComboBox.currentIndexChanged.connect(self.replot)
 
-    def addCurve(self, curve: Curve, xraw: bool, yraw: bool, **kwargs):
-        assert isinstance(curve, Curve)
-        curve = curve.sanitize()
-        self.curves.append((curve, kwargs, xraw, yraw))
-        logger.debug('Curve elements types: q: {}, Intensity: {}, Error: {}, qError: {}'.format(type(curve.q),
-                                                                                                type(curve.Intensity),
-                                                                                                type(curve.Error),
-                                                                                                type(curve.qError)))
-        logger.debug('Added a curve.')
-
-    def on_plotTypeComboBox_currentIndexChanged(self):
+    def pixelOrQChanged(self, state: bool):
+        self.pixelOrQToolButton.setText('q' if state else 'Pixel')
         self.replot()
+
+    def addCurve(self, curve: Curve, **kwargs):
+        assert isinstance(curve, Curve)
+        self.curves.append((curve, kwargs))
+        logger.debug('Added a curve.')
 
     def on_showErrorBarsToolButton_toggled(self):
         self.replot()
 
-    def on_showGridToolButton_toggled(self):
+    def showGrid(self, grid: bool):
         self.axes.grid(self.showGridToolButton.isChecked(), which='both')
         self.canvas.draw()
 
-    def on_showLegendToolButton_toggled(self):
-        self.axes.get_legend().set_visible(self.showLegendToolButton.isChecked())
+    def showLegend(self, show: bool):
+        self.axes.get_legend().set_visible(show)
         self.canvas.draw()
-
-    def on_showLinesToolButton_toggled(self):
-        self.replot()
-
-    def on_symbolsTypeComboBox_currentIndexChanged(self):
-        self.replot()
 
     def replot(self):
         logger.debug('Replotting curves')
-        self.figure.clear()
-        self.axes = self.figure.add_subplot(1, 1, 1)
+        self.axes.clear()
         for i, (curve, kwargs) in enumerate(self.curves):
             if curve is None:
                 # curve is temporarily absent.
                 continue
             kwargs = kwargs.copy()  # do not change the original
-            x = curve.q
-            y = curve.Intensity
-            dx = curve.qError
-            dy = curve.Error
+            x = curve.q if self.pixelOrQToolButton.isChecked() else curve.pixel
+            y = curve.intensity
+            dx = curve.quncertainty if self.pixelOrQToolButton.isChecked() else np.zeros_like(x)
+            dy = curve.uncertainty
             if self.plotTypeComboBox.currentText() == 'Kratky':
-                dy = ((curve.q ** 2 * curve.Error) ** 2 + (2 * curve.Intensity * curve.q * curve.qError) ** 2) ** 0.5
-                y = curve.Intensity * curve.q ** 2
+                dy = ((x ** 2 * dy) ** 2 + (2 * y * x * dx) ** 2) ** 0.5
+                y = y * x ** 2
             elif self.plotTypeComboBox.currentText() == 'Porod':
-                dy = ((curve.q ** 4 * curve.Error) ** 2 + (
-                        4 * curve.Intensity * curve.q ** 3 * curve.qError) ** 2) ** 0.5
-                y = curve.Intensity * curve.q ** 4
+                dy = ((x ** 4 * dy) ** 2 + (
+                        4 * y * x ** 3 * dx) ** 2) ** 0.5
+                y = y * x ** 4
             else:
                 # do nothing
                 pass
@@ -158,10 +155,10 @@ class PlotCurve(QtWidgets.QWidget, Ui_Form):
         if self.showGridToolButton.isChecked():
             self.axes.grid(True, which='both')
 
-        self.axes.set_xlabel(r'$q$ (nm$^{-1}$)')
-        self.axes.set_ylabel(r'$d\Sigma/d\Omega$ (cm$^{-1}$ sr$^{-1}$)')
+        self.axes.set_xlabel(r'$q$ (nm$^{-1}$)' if self.pixelOrQToolButton.isChecked() else 'Distance from origin (pixel)')
+        self.axes.set_ylabel(r'$d\Sigma/d\Omega$ (cm$^{-1}$ sr$^{-1}$)' if self.pixelOrQToolButton.isChecked() else 'Intensity')
         # ToDo: draw logo
-        self.canvas.draw()
+        self.canvas.draw_idle()
         self.navigationToolbar.update()
 
     def savefig(self, filename: str, **kwargs):
@@ -180,9 +177,13 @@ class PlotCurve(QtWidgets.QWidget, Ui_Form):
         self.curves = []
         self.replot()
 
-    def isRawMode(self) -> bool:
-        return not self.plotTypeComboBox.isVisible()
+    def isPixelMode(self) -> bool:
+        return not self.pixelOrQToolButton.isChecked()
 
-    def setRawMode(self, rawmode: bool):
-        self.plotTypeComboBox.setVisible(not rawmode)
+    def setPixelMode(self, rawmode: bool):
+        if rawmode != (not self.pixelOrQToolButton.isChecked()):
+            self.pixelOrQToolButton.toggle()
         self.replot()
+
+    def getRange(self) -> Tuple[float, float, float, float]:
+        return self.axes.axis()
