37a3181e78d670cd84ce50b801958b83e8fccc6d
András Wacha <awacha@gmail.com>
2020-10-09 15:46:16 +0200

    Flag commands corrected

--- cct/core2/commands/flags.py
+++ cct/core2/commands/flags.py
@@ -10,22 +10,25 @@ class NewFlag(InstantCommand):
     arguments = [StringArgument('flag', 'Name of the flag')]
 
     def run(self, arguments: Any) -> Any:
+        self.message.emit(f'Creating a new flag with name "{arguments}"')
         self.instrument.interpreter.flags.addFlag(arguments, False)
 
 
-class SetFlag(Command):
+class SetFlag(InstantCommand):
     name = 'setflag'
     description = 'Set a flag to True'
     arguments = [StringArgument('flag', 'Name of the flag')]
 
     def run(self, arguments: Any) -> Any:
+        self.message.emit(f'Setting flag "{arguments}" to true')
         self.instrument.interpreter.flags.setFlag(arguments, True)
 
 
-class ClearFlag(Command):
+class ClearFlag(InstantCommand):
     name = 'clearflag'
     description = 'Clear the flag (set it to False)'
     arguments = [StringArgument('flag', 'Name of the flag')]
 
     def run(self, arguments: Any) -> Any:
+        self.message.emit(f'Clearing flag "{arguments}"')
         self.instrument.interpreter.flags.setFlag(arguments, False)
