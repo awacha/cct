5e890cb75c2bdaced3e880a668cc78b839c03049
András Wacha <awacha@gmail.com>
2020-11-04 09:51:18 +0100

    Improved command-line handling

--- cct/__main__.py
+++ cct/__main__.py
@@ -1,4 +1,5 @@
 import logging
+import pickle
 import sys
 import traceback
 import multiprocessing
@@ -11,7 +12,10 @@ from PyQt5 import QtWidgets
 from cct.core2.instrument.instrument import Instrument
 from cct.qtgui2.main.mainwindow import MainWindow
 from cct.qtgui2.main.logindialog import LoginDialog
+from cct.core2.config import Config
 import cct.qtgui2.processingmain
+from cct.core2.instrument.components.datareduction.datareductionpipeline import DataReductionPipeLine
+from cct.core2.instrument.components.io import IO
 
 # logging.basicConfig()
 logging.root.setLevel(logging.DEBUG)
@@ -38,10 +42,11 @@ def main():
 
 
 @main.command()
-@click.option('--config', default='config/cct.pickle', help='Config file')
-@click.option('--online', default=False, help='Connect to devices', type=bool, is_flag=True)
-@click.option('--root', default=False, help='Skip login', type=bool, is_flag=True)
+@click.option('--config', '-c', default='config/cct.pickle', help='Config file', type=click.Path(exists=True, file_okay=True, dir_okay=False, writable=False, readable=True, allow_dash=False, ))
+@click.option('--online/--offline', default=False, help='Connect to devices')
+@click.option('--root/--no-root', '-r', default=False, help='Skip login', type=bool, is_flag=True)
 def daq(config:str, online: bool, root: bool):
+    """Open the data acquisition GUI mode"""
     multiprocessing.set_start_method('forkserver')  # the default 'fork' method is not appropriate for multi-threaded programs, e.g. with PyQt.
     sys.excepthook = excepthook
     app = QtWidgets.QApplication(sys.argv)
@@ -85,8 +90,9 @@ def daq(config:str, online: bool, root: bool):
 
 
 @main.command()
-@click.option('--project', default=None, help='Project file to load')
+@click.option('--project', '-p', default=None, help='Project file to load')
 def processing(project: Optional[str]):
+    """Open the data processing GUI"""
     multiprocessing.set_start_method('forkserver')  # the default 'fork' method is not appropriate for multi-threaded programs, e.g. with PyQt.
     sys.excepthook = excepthook
     app = QtWidgets.QApplication(sys.argv)
@@ -98,6 +104,55 @@ def processing(project: Optional[str]):
     app.deleteLater()
     sys.exit(result)
 
+
+@main.command()
+@click.option('--firstfsn', '-f', default=None, help='First file sequence number', type=click.IntRange(0), prompt='First file sequence number')
+@click.option('--lastfsn', '-l', default=None, help='Last file sequence number', type=click.IntRange(0), prompt='Last file sequence number')
+@click.option('--config', '-c', default='config/cct.pickle', help='Config file', type=click.Path(exists=True, file_okay=True, dir_okay=False, writable=False, readable=True, allow_dash=False, ))
+def datareduction(firstfsn: int, lastfsn: int, config):
+    """Command-line data reduction routine"""
+    config = Config(dicorfile=config)
+    config.filename = None  # inhibit autosave
+    io = IO(config=config, instrument=None)
+    pipeline = DataReductionPipeLine(config.asdict())
+    for fsn in range(firstfsn, lastfsn+1):
+        try:
+            ex = io.loadExposure(config['path']['prefixes']['crd'], fsn, raw=True, check_local=True)
+        except FileNotFoundError:
+            logger.warning(f'Cannot load exposure #{fsn}')
+            continue
+        pipeline.process(ex)
+
+
+@main.command()
+@click.option('--config', '-c', default='config/cct.pickle', help='Config file', type=click.Path(exists=True, file_okay=True, dir_okay=False, writable=False, readable=True, allow_dash=False, ))
+@click.option('--maxlevel', '-l', default=None, help='Maximum depth', type=int)
+def dumpconfig(config, maxlevel):
+    """Dump the contents of a config file"""
+    with open(config, 'rb') as f:
+        config = pickle.load(f)
+    indentation='  '
+    if not isinstance(maxlevel, int):
+        maxlevel = -1
+    if maxlevel < 0:
+        maxlevel = None
+
+    def dump(conf, level:int):
+        if (maxlevel is not None) and (level > maxlevel):
+            return
+        if not isinstance(conf, dict):
+            click.echo(f'{level*indentation}- {conf}')
+            return
+        for key in sorted(conf):
+            if isinstance(conf[key], dict):
+                click.echo(f'{level*indentation}{key}:')
+                dump(conf[key], level+1)
+            elif isinstance(conf[key], list):
+                for entry in conf[key]:
+                    dump(entry, level+1)
+            else:
+                click.echo(f'{indentation*level}{key}: {conf[key]}')
+    dump(config, 0)
 
 
 if __name__ == '__main__':
