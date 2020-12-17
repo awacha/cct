335298a88e98aeeb0f7e6d4f8c1715e96fb9a586
András Wacha <awacha@gmail.com>
2020-12-17 17:22:24 +0100

    Matrix averager: "Standard error of the mean" method

--- cct/core2/algorithms/matrixaverager.py
+++ cct/core2/algorithms/matrixaverager.py
@@ -1,7 +1,11 @@
 import enum
-from typing import Tuple
+import logging
+from typing import Tuple, Optional
 
 import numpy as np
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 class ErrorPropagationMethod(enum.Enum):
@@ -23,21 +27,24 @@ class ErrorPropagationMethod(enum.Enum):
     Linear = 1
     Gaussian = 2
     Conservative = 3
+    StandardErrorOfTheMean = 4
 
 
 class MatrixAverager:
-    value: np.ndarray = None
-    value2: np.ndarray = None
-    error: np.ndarray = None
+    value: Optional[np.ndarray] = None
+    value2: Optional[np.ndarray] = None
+    error: Optional[np.ndarray] = None
     count: int = 0
     method: ErrorPropagationMethod
 
     def __init__(self, errorpropagationmethod: ErrorPropagationMethod):
         self.method = errorpropagationmethod
+        self.count = 0
 
     def add(self, value: np.ndarray, error: np.ndarray):
         error = self.fixBadValues(error)
         if self.value is None:
+            assert (self.value2 is None) and (self.error is None)
             if self.method == ErrorPropagationMethod.Weighted:
                 self.value = value / error ** 2
                 self.error = 1 / error ** 2
@@ -50,6 +57,10 @@ class MatrixAverager:
             elif self.method == ErrorPropagationMethod.Conservative:
                 self.error = error ** 2
                 self.value = value * 1  # Multiplication by 1: make a copy
+                self.value2 = value ** 2
+            elif self.method == ErrorPropagationMethod.StandardErrorOfTheMean:
+                self.error = error  # don't use it
+                self.value = value * 1
                 self.value2 = value ** 2
         else:
             if self.method == ErrorPropagationMethod.Weighted:
@@ -65,9 +76,17 @@ class MatrixAverager:
                 self.value += value
                 self.error += error ** 2
                 self.value2 += value ** 2
+            elif self.method == ErrorPropagationMethod.StandardErrorOfTheMean:
+                self.value += value
+                self.value2 += value ** 2
         self.count += 1
 
     def get(self) -> Tuple[np.ndarray, np.ndarray]:
+        if not self.count:
+            raise ValueError('Cannot get average: no data given yet.')
+        logger.debug(f'Returning averaged matrices according to method {self.method}. {self.count=}')
+        assert self.value is not None
+        assert self.error is not None
         if self.method == ErrorPropagationMethod.Weighted:
             return self.value / self.error, 1 / self.error ** 0.5
         elif self.method == ErrorPropagationMethod.Linear:
@@ -75,10 +94,15 @@ class MatrixAverager:
         elif self.method == ErrorPropagationMethod.Gaussian:
             return self.value / self.count, self.error ** 0.5 / self.count
         elif self.method == ErrorPropagationMethod.Conservative:
-            error_std = (self.value2 - self.value ** 2 / self.count) / (
-                    self.count - 1) / self.count ** 0.5 if self.count > 1 else np.zeros_like(self.value)
+            # Standard error of the mean or propagated error, whichever is largest
+            error_sem = ((self.value2 - self.value ** 2 / self.count) / (
+                    self.count - 1)) ** 0.5 / self.count ** 0.5 if self.count > 1 else np.zeros_like(self.value)
             error_propagated = self.error ** 0.5 / self.count
-            return self.value / self.count, np.stack((error_std, error_propagated)).max(axis=0)
+            return self.value / self.count, np.stack((error_sem, error_propagated)).max(axis=0)
+        elif self.method == ErrorPropagationMethod.StandardErrorOfTheMean:
+            return self.value / self.count, \
+                   ((1 / (self.count - 1) / self.count * (self.value2 - self.value ** 2 / self.count)) ** 0.5) \
+                       if self.count > 1 else np.zeros_like(self.value)
 
     @staticmethod
     def fixBadValues(matrix: np.ndarray) -> np.ndarray:
