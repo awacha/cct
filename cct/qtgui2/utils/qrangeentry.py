948cc83a6c7e04d8e5c988741eee47ffa6f60dea
András Wacha <awacha@gmail.com>
2020-12-17 17:29:30 +0100

    QRangeEntry and ValueAndUncertaintyEntry

---
+++ cct/qtgui2/utils/qrangeentry.py
@@ -0,0 +1,72 @@
+from typing import Tuple, Optional
+
+from PyQt5 import QtWidgets, QtCore
+from .qrangeentry_ui import Ui_Form
+
+
+class QRangeEntry(QtWidgets.QWidget, Ui_Form):
+    valueChanged = QtCore.pyqtSignal(float, float, int)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.setupUi(self)
+
+    def setupUi(self, Form):
+        super().setupUi(Form)
+        self.qminDoubleSpinBox.valueChanged.connect(self.qmaxDoubleSpinBox.setMinimum)
+        self.qmaxDoubleSpinBox.valueChanged.connect(self.qminDoubleSpinBox.setMaximum)
+        self.qminDoubleSpinBox.valueChanged.connect(self._onvaluechanged)
+        self.qmaxDoubleSpinBox.valueChanged.connect(self._onvaluechanged)
+        self.qCountSpinBox.valueChanged.connect(self._onvaluechanged)
+
+    def _onvaluechanged(self):
+        self.valueChanged.emit(self.qminDoubleSpinBox.value(), self.qmaxDoubleSpinBox.value(), self.qCountSpinBox.value())
+
+    def setMinimum(self, value: float):
+        self.qminDoubleSpinBox.setMinimum(value)
+
+    def setMaximum(self, value: float):
+        self.qmaxDoubleSpinBox.setMaximum(value)
+
+    def setDecimals(self, prec: int):
+        self.qminDoubleSpinBox.setDecimals(prec)
+        self.qmaxDoubleSpinBox.setDecimals(prec)
+
+    def setFrame(self, showframe: bool):
+        self.qminDoubleSpinBox.setFrame(showframe)
+        self.qmaxDoubleSpinBox.setFrame(showframe)
+        self.qCountSpinBox.setFrame(showframe)
+
+    def setKeyboardTracking(self, kt: bool):
+        self.qminDoubleSpinBox.setKeyboardTracking(kt)
+        self.qmaxDoubleSpinBox.setKeyboardTracking(kt)
+        self.qCountSpinBox.setKeyboardTracking(kt)
+
+    def keyboardTracking(self):
+        assert self.qmaxDoubleSpinBox.keyboardTracking() == self.qminDoubleSpinBox.keyboardTracking() == self.qCountSpinBox.keyboardTracking()
+        return self.qmaxDoubleSpinBox.keyboardTracking()
+
+    def hasFrame(self):
+        assert self.qmaxDoubleSpinBox.hasFrame() == self.qminDoubleSpinBox.hasFrame() == self.qCountSpinBox.hasFrame()
+        return self.qmaxDoubleSpinBox.hasFrame()
+
+    def value(self) -> Tuple[float, float, int]:
+        return self.qminDoubleSpinBox.value(), self.qmaxDoubleSpinBox.value(), self.qCountSpinBox.value()
+
+    def minimum(self) -> float:
+        return self.qminDoubleSpinBox.minimum()
+
+    def maximum(self) -> float:
+        return self.qmaxDoubleSpinBox.maximum()
+
+    def setValue(self, qmin: Optional[float], qmax: Optional[float], count: Optional[int]):
+        if qmin is not None:
+            self.qminDoubleSpinBox.setValue(qmin)
+        if qmax is not None:
+            self.qmaxDoubleSpinBox.setValue(qmax)
+        if count is not None:
+            self.qCountSpinBox.setValue(count)
+
+    def setRange(self, min: float, max: float):
+        self.qminDoubleSpinBox.setMinimum(min)
+        self.qmaxDoubleSpinBox.setMaximum(max)
---
+++ cct/qtgui2/utils/valueanduncertaintyentry.py
@@ -0,0 +1,70 @@
+from typing import Tuple, Optional
+
+from PyQt5 import QtWidgets, QtCore
+from .valueanduncertaintyentry_ui import Ui_Form
+
+
+class ValueAndUncertaintyEntry(QtWidgets.QWidget, Ui_Form):
+    valueChanged = QtCore.pyqtSignal(float, float)
+    maxrelativeuncertainty: float = 2  # 1 means 100%
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.setupUi(self)
+
+    def setupUi(self, Form):
+        super().setupUi(Form)
+        self.uncertaintyDoubleSpinBox.setMinimum(0)
+        self.uncertaintyDoubleSpinBox.setMaximum(self.valueDoubleSpinBox.maximum()*self.maxrelativeuncertainty)
+        self.valueDoubleSpinBox.valueChanged.connect(self._onvaluechanged)
+        self.uncertaintyDoubleSpinBox.valueChanged.connect(self._onvaluechanged)
+
+    def _onvaluechanged(self):
+        self.valueChanged.emit(self.valueDoubleSpinBox.value(), self.uncertaintyDoubleSpinBox.value())
+
+    def setMinimum(self, value: float):
+        self.valueDoubleSpinBox.setMinimum(value)
+
+    def setMaximum(self, value: float):
+        self.valueDoubleSpinBox.setMaximum(value)
+        self.uncertaintyDoubleSpinBox.setMaximum(value * self.maxrelativeuncertainty)
+
+    def setDecimals(self, prec: int):
+        self.valueDoubleSpinBox.setDecimals(prec)
+        self.uncertaintyDoubleSpinBox.setDecimals(prec)
+
+    def setFrame(self, showframe: bool):
+        self.valueDoubleSpinBox.setFrame(showframe)
+        self.uncertaintyDoubleSpinBox.setFrame(showframe)
+
+    def setKeyboardTracking(self, kt: bool):
+        self.valueDoubleSpinBox.setKeyboardTracking(kt)
+        self.uncertaintyDoubleSpinBox.setKeyboardTracking(kt)
+
+    def keyboardTracking(self):
+        assert self.uncertaintyDoubleSpinBox.keyboardTracking() == self.valueDoubleSpinBox.keyboardTracking()
+        return self.uncertaintyDoubleSpinBox.keyboardTracking()
+
+    def hasFrame(self):
+        assert self.uncertaintyDoubleSpinBox.hasFrame() == self.valueDoubleSpinBox.hasFrame()
+        return self.uncertaintyDoubleSpinBox.hasFrame()
+
+    def value(self) -> Tuple[float, float, int]:
+        return self.valueDoubleSpinBox.value(), self.uncertaintyDoubleSpinBox.value()
+
+    def minimum(self) -> float:
+        return self.valueDoubleSpinBox.minimum()
+
+    def maximum(self) -> float:
+        return self.valueDoubleSpinBox.maximum()
+
+    def setValue(self, value: Optional[float], uncertainty: Optional[float]):
+        if value is not None:
+            self.valueDoubleSpinBox.setValue(value)
+        if uncertainty is not None:
+            self.uncertaintyDoubleSpinBox.setValue(uncertainty)
+
+    def setRange(self, min: float, max: float):
+        self.valueDoubleSpinBox.setMinimum(min)
+        self.valueDoubleSpinBox.setMaximum(max)
+        self.uncertaintyDoubleSpinBox.setMaximum(max*self.maxrelativeuncertainty)
