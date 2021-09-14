6767a45993c0f496fb921c544535e36b75ccdf71
András Wacha <awacha@gmail.com>
2021-09-14 11:18:40 +0200

    Made beam weighting ignore negative intensities

--- cct/core2/algorithms/beamweighting.pyx
+++ cct/core2/algorithms/beamweighting.pyx
@@ -1,8 +1,17 @@
 # cython: cdivision=True, wraparound=False, boundscheck=False, language_level=3, embedsignature=True
-from libc.math cimport sqrt
+from libc.math cimport sqrt, NAN
 from libc.stdint cimport uint8_t
 
 def beamweights(double [:,:] image not None, uint8_t [:,:] mask not None):
+    """Calculate some statistics on an image
+
+    :param image: the scattering image
+    :type image: 2D array, double dtype
+    :param mask: the mask: 0 is masked, nonzero is valid (not masked) pixel
+    :type mask: 2D array, uint8 dtype
+    :return: sum, max, center_row, center_col, sigma_row, sigma_col, pixelcount
+    :rtype: tuple of (float, float, float, float, float, float, int)
+    """
     cdef:
         Py_ssize_t irow, icol
         double meanrow=0, meanrow2=0, meancol=0, meancol2=0, sumimage=0, maximage=0
@@ -11,6 +20,8 @@ def beamweights(double [:,:] image not None, uint8_t [:,:] mask not None):
     maximage=image[0,0]
     for irow in range(0, image.shape[0]):
         for icol in range(0, image.shape[1]):
+            if image[irow, icol] <=0:
+                continue
             if mask[irow, icol] == 0:
                 continue
             if image[irow,icol] > maximage:
@@ -21,4 +32,7 @@ def beamweights(double [:,:] image not None, uint8_t [:,:] mask not None):
             meancol += icol * image[irow, icol]
             meancol2 += icol * icol* image[irow, icol]
             pixelcount += 1
-    return sumimage, maximage, meanrow/sumimage, meancol/sumimage, sqrt((meanrow2 / sumimage - meanrow) / sumimage), sqrt((meancol2 / sumimage - meancol) / sumimage), pixelcount
+    if pixelcount == 0:
+        return NAN, NAN, NAN, NAN, NAN, NAN, 0
+    else:
+        return sumimage, maximage, meanrow/sumimage, meancol/sumimage, sqrt((meanrow2 / sumimage - meanrow) / sumimage), sqrt((meancol2 / sumimage - meancol) / sumimage), pixelcount
