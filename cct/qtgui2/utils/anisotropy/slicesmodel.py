ff4936bdcc0a6916fbce384040ec45cca4c26550
András Wacha <awacha@gmail.com>
2022-08-30 15:41:04 +0200

    Anisotropy UI tuning, might be broken now

--- cct/qtgui2/utils/anisotropy/slicesmodel.py
+++ cct/qtgui2/utils/anisotropy/slicesmodel.py
@@ -32,7 +32,7 @@ class SectorModel(QtCore.QAbstractItemModel):
         if not colors:
             colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22',
                       '#17becf']
-        self.color_iterator = itertools.cycle(self.color_iterator)
+        self.color_iterator = itertools.cycle(colors)
 
     def index(self, row: int, column: int, parent: QtCore.QModelIndex = ...) -> QtCore.QModelIndex:
         return self.createIndex(row, column, None)
