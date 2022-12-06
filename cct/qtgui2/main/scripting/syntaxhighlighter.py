55b6d5a3e8d8947a3074aa09cfd03374c67b32bd
András Wacha <awacha@gmail.com>
2022-12-06 14:56:25 +0100

    Syntax highlighting in scripting works as intended

--- cct/qtgui2/main/scripting/syntaxhighlighter.py
+++ cct/qtgui2/main/scripting/syntaxhighlighter.py
@@ -11,24 +11,27 @@ class ScriptSyntaxHighlighter(QtGui.QSyntaxHighlighter):
 
     def __init__(self, textdocument: QtGui.QTextDocument):
         super().__init__(textdocument)
-        f = QtGui.QTextCharFormat()
-        f.setFontItalic(True)
-        f.setForeground(QtCore.Qt.GlobalColor.darkCyan)
-        self.formats.append(([re.compile("(?P<delimiter>\"\').+(?P=delimiter)")], f))
+        # character format for commands
         f = QtGui.QTextCharFormat()
         f.setFontWeight(QtGui.QFont.Weight.Bold)
         f.setForeground(QtCore.Qt.GlobalColor.darkMagenta)
         self.formats.append(([re.compile(r'\b' + c.name + r'\b') for c in Command.subclasses() if isinstance(c.name, str)], f))
+        # character format for comments
         f = QtGui.QTextCharFormat()
         f.setForeground(QtCore.Qt.GlobalColor.lightGray)
         self.formats.append(([re.compile('#.*$')], f))
+        # character format for labels
         f = QtGui.QTextCharFormat()
         f.setForeground(QtCore.Qt.GlobalColor.blue)
         self.formats.append(([re.compile(r'^\s*@.*$')], f))
+        # character format for strings
+        f = QtGui.QTextCharFormat()
+        f.setFontItalic(True)
+        f.setForeground(QtCore.Qt.GlobalColor.darkCyan)
+        self.formats.append(([re.compile("(?P<delimiter>[\"\']).*?(?P=delimiter)")], f))
 
     def highlightBlock(self, text: str) -> None:
         for regexes, textcharformat in self.formats:
             for regex in regexes:
                 for m in regex.finditer(text):
                     self.setFormat(m.start(), m.end() - m.start(), textcharformat)
-                    break
