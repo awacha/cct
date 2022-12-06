83c97ef15a52102ce1593f0cf6a4a6360f63141b
András Wacha <awacha@gmail.com>
2022-12-06 14:56:11 +0100

    New beamstop calibrator UI

--- cct/qtgui2/devices/motors/beamstopcalibrator.py
+++ cct/qtgui2/devices/motors/beamstopcalibrator.py
@@ -12,69 +12,95 @@ class BeamStopCalibrator(WindowRequiresDevices, QtWidgets.QWidget, Ui_Form):
 
     def setupUi(self, Form):
         super().setupUi(Form)
-        self.calibrateBeamStopPushButton.clicked.connect(self.calibrateBeamStopPosition)
-        self.inRadioButton.toggled.connect(self.inOutChanged)
-        self.fetchXFromMotorToolButton.clicked.connect(self.fetchXFromMotor)
-        self.fetchYFromMotorToolButton.clicked.connect(self.fetchYFromMotor)
-        self.resetXToolButton.clicked.connect(self.loadX)
-        self.resetYToolButton.clicked.connect(self.loadY)
 
     @Slot(bool)
-    def inOutChanged(self, isin: bool):
-        self.loadX()
-        self.loadY()
-
-    @Slot()
-    def fetchXFromMotor(self):
+    def on_fetchXInFromMotorToolButton_clicked(self, checked: bool):
         try:
-            self.xDoubleSpinBox.setValue(self.instrument.beamstop.motorx.where())
+            self.xInDoubleSpinBox.setValue(self.instrument.beamstop.motorx.where())
         except Exception as exc:
             QtWidgets.QMessageBox.critical(self.window(), 'Cannot get X motor coordinate', exc.args[0])
 
-    @Slot()
-    def fetchYFromMotor(self):
+    @Slot(bool)
+    def on_fetchXOutFromMotorToolButton_clicked(self, checked: bool):
         try:
-            self.yDoubleSpinBox.setValue(self.instrument.beamstop.motory.where())
+            self.xOutDoubleSpinBox.setValue(self.instrument.beamstop.motorx.where())
+        except Exception as exc:
+            QtWidgets.QMessageBox.critical(self.window(), 'Cannot get X motor coordinate', exc.args[0])
+
+    @Slot(bool)
+    def on_fetchYInFromMotorToolButton_clicked(self, checked: bool):
+        try:
+            self.yInDoubleSpinBox.setValue(self.instrument.beamstop.motory.where())
         except Exception as exc:
             QtWidgets.QMessageBox.critical(self.window(), 'Cannot get Y motor coordinate', exc.args[0])
 
-    @Slot()
-    def loadX(self):
-        self.xDoubleSpinBox.setValue(self.instrument.beamstop.inPosition()[0] if self.inRadioButton.isChecked() else
-                                     self.instrument.beamstop.outPosition()[0])
+    @Slot(bool)
+    def on_fetchYOutFromMotorToolButton_clicked(self, checked: bool):
+        try:
+            self.yOutDoubleSpinBox.setValue(self.instrument.beamstop.motory.where())
+        except Exception as exc:
+            QtWidgets.QMessageBox.critical(self.window(), 'Cannot get Y motor coordinate', exc.args[0])
 
-    @Slot()
-    def loadY(self):
-        self.yDoubleSpinBox.setValue(self.instrument.beamstop.inPosition()[1] if self.inRadioButton.isChecked() else
-                                     self.instrument.beamstop.outPosition()[1])
+    @Slot(bool)
+    def on_resetXInToolButton_clicked(self, checked: bool):
+        self.xInDoubleSpinBox.setValue(self.instrument.beamstop.inPosition()[0])
 
-    @Slot()
-    def calibrateBeamStopPosition(self):
+    @Slot(bool)
+    def on_resetXOutToolButton_clicked(self, checked: bool):
+        self.xOutDoubleSpinBox.setValue(self.instrument.beamstop.outPosition()[0])
+
+    @Slot(bool)
+    def on_resetYInToolButton_clicked(self, checked: bool):
+        self.yInDoubleSpinBox.setValue(self.instrument.beamstop.inPosition()[1])
+
+    @Slot(bool)
+    def on_resetYOutToolButton_clicked(self, checked: bool):
+        self.yOutDoubleSpinBox.setValue(self.instrument.beamstop.outPosition()[1])
+
+    @Slot(bool)
+    def on_saveInToolButton_clicked(self, checked: bool):
+        self.savePosition("in")
+
+    @Slot(bool)
+    def on_saveOutToolButton_clicked(self, checked: bool):
+        self.savePosition("out")
+
+    def savePosition(self, inorout: str):
+        if inorout == 'in':
+            x = self.xInDoubleSpinBox.value()
+            y = self.yInDoubleSpinBox.value()
+        elif inorout == 'out':
+            x = self.xOutDoubleSpinBox.value()
+            y = self.yOutDoubleSpinBox.value()
+        else:
+            raise ValueError(inorout)
         if QtWidgets.QMessageBox.question(
                 self.window(),
                 'Confirm calibrating beam-stop position',
-                f'Do you really want to set beam-stop {"in" if self.inRadioButton.isChecked() else "out"} position '
-                f'to ({self.xDoubleSpinBox.value():.4f}, {self.yDoubleSpinBox.value():.4f})?',
+                f'Do you really want to set beam-stop {inorout} position '
+                f'to ({x:.4f}, {y:.4f})?',
                 QtWidgets.QMessageBox.StandardButton.Yes | QtWidgets.QMessageBox.StandardButton.No,
                 QtWidgets.QMessageBox.StandardButton.No
         ) == QtWidgets.QMessageBox.StandardButton.No:
             QtWidgets.QMessageBox.information(
                 self.window(),
                 'Calibrating beam-stop position',
-                f'Beam-stop {"in" if self.inRadioButton.isChecked() else "out"} position NOT SET.')
+                f'Beam-stop {inorout} position NOT SET.')
             return
         try:
-            if self.inRadioButton.isChecked():
-                self.instrument.beamstop.calibrateIn(self.xDoubleSpinBox.value(), self.yDoubleSpinBox.value())
-            else:
-                self.instrument.beamstop.calibrateOut(self.xDoubleSpinBox.value(), self.yDoubleSpinBox.value())
+            self.instrument.beamstop.calibrateIn(x, y)
         except Exception as exc:
             QtWidgets.QMessageBox.critical(
                 self.window(), 'Calibrating beam-stop position',
-                f'Error while calibrating beam-stop {"in" if self.inRadioButton.isChecked() else "out"} position: '
+                f'Error while calibrating beam-stop {inorout} position: '
                 f'{exc.args[0]}.')
         else:
+            if inorout == "in":
+                newposition = self.instrument.beamstop.inPosition()
+            else:
+                assert inorout == "out"
+                newposition = self.instrument.beamstop.outPosition()
             QtWidgets.QMessageBox.information(
                 self.window(), 'Calibrating beam-stop position',
-                f'Beam-stop {"in" if self.inRadioButton.isChecked() else "out"} position successfully set to '
-                f'{self.instrument.beamstop.inPosition() if self.inRadioButton.isChecked() else self.instrument.beamstop.outPosition()}')
+                f'Beam-stop in position successfully set to '
+                f'{newposition}')
