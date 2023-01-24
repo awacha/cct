95f1829197189ddc7c0f656784ad37674c9b5d77
András Wacha <awacha@gmail.com>
2023-01-24 14:52:03 +0100

    Sequence wizard debugging

--- cct/qtgui2/main/scripting/wizard/sequencewizard/endpage.py
+++ cct/qtgui2/main/scripting/wizard/sequencewizard/endpage.py
@@ -10,10 +10,10 @@ class EndPage(QtWidgets.QWizardPage, Ui_WizardPage):
     def setupUi(self, WizardPage):
         super().setupUi(WizardPage)
         self.xRaySourcePowerModeComboBox.addItems(['off', 'standby', 'full'])
-        self.registerField('iterationCount', self.iterationCountSpinBox, b'value', b'valueChanged')
-        self.registerField('userBreakFlag', self.addUserBreakFlagCheckBox, b'checked', b'toggled')
-        self.registerField('closeShutterAfter', self.closeShutterCheckBox, b'checked', b'toggled')
-        self.registerField('setXraySourcePowerAfter', self.setXraySourcePowerModeCheckBox, b'checked', b'toggled')
-        self.registerField('setXraySourcePowerAfterTo', self.xRaySourcePowerModeComboBox, b'currentText', b'currentTextChanged')
+        self.registerField('iterationCount', self.iterationCountSpinBox, 'value', 'valueChanged')
+        self.registerField('userBreakFlag', self.addUserBreakFlagCheckBox, 'checked', 'toggled')
+        self.registerField('closeShutterAfter', self.closeShutterCheckBox, 'checked', 'toggled')
+        self.registerField('setXraySourcePowerAfter', self.setXraySourcePowerModeCheckBox, 'checked', 'toggled')
+        self.registerField('setXraySourcePowerAfterTo', self.xRaySourcePowerModeComboBox, 'currentText', 'currentTextChanged')
 
 
