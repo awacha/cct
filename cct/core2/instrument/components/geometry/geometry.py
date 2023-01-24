ca10959b29d3fbfe790b36a167b9518eb21033b4
András Wacha <awacha@gmail.com>
2023-01-24 09:05:17 +0100

    Geometry component debugging

--- cct/core2/instrument/components/geometry/geometry.py
+++ cct/core2/instrument/components/geometry/geometry.py
@@ -108,6 +108,7 @@ class Geometry(Component, QtCore.QObject):
     def onConfigChanged(self, path, value):
         if (len(path) < 2) or (path[0] != 'geometry'):
             return
+        logger.debug(f'Geometry config changed: {path=}, {value=}, {path[1]=}')
         if path[1] in ['l1_elements', 'l2_elements', 'l1base', 'isoKFspacer', 'pinhole_1', 'pinhole_2', 'pinhole_3',
                        'dist_sample_det', 'ph3tosample', 'beamstoptodetector', 'beamstop', 'wavelength']:
             self.recalculateDerivedParameters()
